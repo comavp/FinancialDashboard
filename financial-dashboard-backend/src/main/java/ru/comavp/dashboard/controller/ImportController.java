80b0fbba623109dce3a4560f8211ac9de5f18efe
comavp <comavp@yandex.ru>
2024-07-17 20:05:31 +0400

    Refactor requests mapping values

--- financial-dashboard-backend/src/main/java/ru/comavp/dashboard/controller/ImportController.java
+++ financial-dashboard-backend/src/main/java/ru/comavp/dashboard/controller/ImportController.java
@@ -14,25 +14,25 @@ import ru.comavp.dashboard.utils.FileUtils;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/import-data")
 @AllArgsConstructor
 public class ImportController {
 
     private ImportService importService;
 
-    @PostMapping(value = "/import-data", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity importAllData(@RequestParam("file") MultipartFile file) throws IOException {
         importService.importAllDataFromWorkBookSheet(FileUtils.getWorkbook(file));
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping(value = "/import-data/invest-transactions", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/invest-transactions", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity importInvestTransactions(@RequestParam("file") MultipartFile file) throws IOException {
         importService.importInvestTransactions(FileUtils.getWorkbook(file));
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping(value = "/import-data/replenishments", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/replenishments", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity importReplenishments(@RequestParam("file") MultipartFile file) throws IOException {
         importService.importReplenishmentTransactions(FileUtils.getWorkbook(file));
         return ResponseEntity.ok().build();
