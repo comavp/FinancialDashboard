f7e6a779e3d47761e602032ce7b1c5727406e925
comavp <comavp@yandex.ru>
2024-09-22 16:36:16 +0400

    Add css styles for Navbar buttons

--- financial-dashboard-frontend/src/components/Navbar.vue
+++ financial-dashboard-frontend/src/components/Navbar.vue
@@ -1,15 +1,70 @@
 <template>
   <div class="navbar">
-    <button @click="$router.push('/investments-portfolio')">Портфель</button>
-    <button @click="$router.push('/invest-transactions')">
+    <button 
+      @click="processNavbarButtonClick('/investments-portfolio')" 
+      :class="{'active': buttonActive('/investments-portfolio')}"
+    >
+      Портфель
+    </button>
+    <button 
+      @click="processNavbarButtonClick('/invest-transactions')" 
+      :class="{'active': buttonActive('/invest-transactions')}"
+    >
       История покупок
     </button>
-    <button @click="$router.push('/replenishments')">История пополнений</button>
+    <button 
+      @click="processNavbarButtonClick('/replenishments')" 
+      :class="{'active': buttonActive('/replenishments')}"
+    >
+      История пополнений
+    </button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      currentPath: '',
+      buttonsStateMap: {
+        '/investments-portfolio': false,
+        '/invest-transactions': false,
+        '/replenishments': false
+      }
+    }
+  },
+  methods: {
+    processNavbarButtonClick(selectedPath) {
+      this.$router.push(selectedPath);
+      if (this.currentPath != '') {
+        this.buttonsStateMap[this.currentPath] = false;
+      }
+      this.buttonsStateMap[selectedPath] = true;
+      this.currentPath = selectedPath;
+    },
+    buttonActive(selectedPath) {
+      return this.buttonsStateMap[selectedPath];
+    }
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  background-color: rgb(246, 248, 250); /* github navbar color */
+  height: 50px;
+  align-items: center;
+}
+button {
+  background: none;
+  border: 0;
+  font-size: 16px;
+  padding: 10px 15px;
+}
+button:hover {
+  background-color: rgb(234, 237, 240); /* github navbar color */
+}
+button.active {
+  font-weight: 700;
+}
+</style>
