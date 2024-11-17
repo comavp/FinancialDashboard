f0928d8ff2c4e8818863db41f6532f3213fc0eea
comavp <comavp@yandex.ru>
2024-11-18 00:04:14 +0400

    Add more information about investment portfolio to UI

--- financial-dashboard-frontend/src/pages/InvestmentsPortfolio.vue
+++ financial-dashboard-frontend/src/pages/InvestmentsPortfolio.vue
@@ -11,7 +11,10 @@ export default {
     return {
       investmentsPortfolioInfo: [],
       columnNames: [
+        'Тикер',
         'Название эмитента',
+        'ISIN',
+        'Категория',
         'Брокер',
         'Количество, шт.',
         'Текущая стоимость'
@@ -28,12 +31,18 @@ export default {
           "http://localhost:8081/api/invest-transactions/portfolio"
         );
         response.data.forEach((element) => {
+          const ticker = element.ticker;
           const issuerName = element.issuerName;
+          const isin = element.isin;
+          const category = element.category;
           const price = element.price;
           const brokerNameToQuantityMap = element.brokerNameToQuantityMap;
           for (let [key, value] of Object.entries(brokerNameToQuantityMap)) {
             const item = {
+              ticker: ticker,
               issuerName: issuerName,
+              isin: isin,
+              category: category,
               brokerName: key,
               quantity: value,
               cost: price * value
